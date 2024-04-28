8281bf25b0b2f86f3ea003508dc12df464f3fdfd
Oso-blonco <168327669+Oso-blonco@users.noreply.github.com>
2024-04-28 14:20:23 -0700

    terminal works now

--- cadenstestworld.cpp
+++ cadenstestworld.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
 using namespace std;
-
+//g++ cadenstestworld.cpp -o test && ./test
 
 int main(){
     int x;
-    cout << "what is your number";
+    cout << "what is your number" << "\n";
   
     cin >> x;
     cout << "your number is " << x;
