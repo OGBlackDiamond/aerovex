89a599d128de84d6f6d01c6fb6a47683b99c45d4
Oso-blonco <168327669+Oso-blonco@users.noreply.github.com>
2024-04-28 22:02:03 -0700

    gave it some functions they do not work properly

--- phycicsfunctions.cpp
+++ phycicsfunctions.cpp
@@ -45,15 +45,31 @@ public:
     pos.y = 0;
     pos.z = 0;
 
-    velo.x = 1;
-    velo.y = 1;
-    velo.z = 1;
+    velo.x = 0;
+    velo.y = 0;
+    velo.z = 0;
 
-    accel.x = 2;
-    accel.y = 2;
-    accel.z = 2;
+    accel.x = 1;
+    accel.y = 1;
+    accel.z = 1;
   }
-
+void getpos(){
+  cout << "(" << pos.x << ","<< pos.y << ","<< pos.z << ",";
+}
+void updateobj(double time){
+updatevelo(time);
+updatepos(time);
+}
+void updatepos(double time){
+pos.x = pos.x + velo.x*time;
+pos.y = pos.y + velo.y*time;
+pos.z = pos.z + velo.z*time;
+}
+void updatevelo(double time){
+velo.x = velo.x + accel.x*time;
+velo.y = velo.y + accel.y*time;
+velo.z = velo.z + accel.z*time;
+}
 };
 
 
@@ -62,12 +78,23 @@ phycicsfunctions engine;
 
 // main methed used for testing will be removed later
 int main(){
-cout << "this is the position" << "\n";
+  double time;
+  int runs;
+  cin >> runs;
+  for (int i = 0; i < runs; i++)
+  {
+    time += .1;
+   engine.updateobj(.1);
+  }
+  engine.getpos();
+  
+/*cout << "this is the position" << "\n";
 cout << engine.pos.x << "," << engine.pos.y << "," << engine.pos.z << "\n";
 cout << "this is the velocity" << "\n";
 cout << engine.velo.x << "," << engine.velo.y << "," << engine.velo.z<< "\n";
 cout << "this is the acceleration" << "\n";
 cout << engine.accel.x << "," << engine.accel.y << "," << engine.accel.z<< "\n";
+*/
 return 0;
 
 }
