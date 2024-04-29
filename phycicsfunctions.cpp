6b67e93e9b5f3aa71e2d37ec53341873d461b2ec
OGBlackDiamond <caden.feller@gmail.com>
2024-04-28 21:18:02 -0700

    fixed struct issue

--- phycicsfunctions.cpp
+++ phycicsfunctions.cpp
@@ -4,40 +4,70 @@ g++ phycicsfunctions.cpp -o test && ./test
 #include <ctgmath>
 #include <iostream>
 using namespace std;
+
+// defines struct shells
+
+struct Position
+{
+  double x;
+  double y;
+  double z;
+};
+struct velocity
+{
+  double x;
+  double y;
+  double z;
+};
+struct acceleration
+{
+  double x;
+  double y;
+  double z;
+};
+
+
 // let the variables begin
 class phycicsfunctions
 {
 public:
-struct 
-{
-  double x = 0;
-  double y = 0;
-  double z = 0;
-} Position;
-struct 
-{
-  double x = 1;
-  double y = 1;
-  double z = 1;
-} velocity;
-struct 
-{
-  double x = 2;
-  double y = 2;
-  double z = 2;
-} acceleration;
+
+  //creates a new instance of the structs
+  Position pos;
+  velocity velo;
+  acceleration accel;
+
+  // constructor for the class
+  phycicsfunctions() {
+
+    // assigns values on construction
+    pos.x = 0;
+    pos.y = 0;
+    pos.z = 0;
+
+    velo.x = 1;
+    velo.y = 1;
+    velo.z = 1;
+
+    accel.x = 2;
+    accel.y = 2;
+    accel.z = 2;
+  }
 
 };
 
 
+// instantiates a new phyfunc class
+phycicsfunctions engine;
+
 // main methed used for testing will be removed later
 int main(){
 cout << "this is the position" << "\n";
-cout << Position.x << "," << Position.y << "," << Position.z << "\n";
+cout << engine.pos.x << "," << engine.pos.y << "," << engine.pos.z << "\n";
 cout << "this is the velocity" << "\n";
-cout << velocity.x << "," << velocity.y << "," << velocity.z<< "\n";
+cout << engine.velo.x << "," << engine.velo.y << "," << engine.velo.z<< "\n";
 cout << "this is the acceleration" << "\n";
-cout << acceleration.x << "," << acceleration.y << "," << acceleration.z<< "\n";
+cout << engine.accel.x << "," << engine.accel.y << "," << engine.accel.z<< "\n";
 return 0;
 
 }
